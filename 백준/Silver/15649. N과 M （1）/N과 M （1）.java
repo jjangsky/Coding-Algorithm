53cf19d7d97d3cd49cb8f74b8ae24368695245e2
짱스키 <103441154+jjangsky@users.noreply.github.com>
2024-12-18 23:38:37 +0900

    [Silver III] Title: N과 M (1), Time: 216 ms, Memory: 22852 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    private static boolean[] visit;
+    private static int[] arr;
+    private static StringBuilder result = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputArr = br.readLine().split(" ");
+        int a = Integer.parseInt(inputArr[0]);
+        int b = Integer.parseInt(inputArr[1]);
+
+        visit = new boolean[a];
+        arr = new int[b];
+
+        dfs(a, b, 0);
+        System.out.println(result);
+
+    }
+
+
+    private static void dfs(int a, int b, int depth){
+        // 최고값에 도달 했을 때 배열 출력
+        if(depth == b){
+            for (int val: arr){
+                result.append(val).append(' ');
+            }
+            result.append('\n');
+            return;
+        }
+
+        for(int i = 0; i < a; i++){
+            if(!visit[i]){
+                visit[i] = true;
+                arr[depth] = i + 1;
+                dfs(a, b, depth+1);
+
+                visit[i] = false;
+            }
+        }
+    }
+}
+
+
